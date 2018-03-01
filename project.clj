9213e5f4a37e59764a4d038d3ed9f7ba8b26ddaa
Mike Richards <mrichards42@gmx.com>
2018-03-01 12:41:48 -0500

    Re-add cljsjs/plotly dependency
    Just for the externs file.  Still loading from a CDN.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.908"]
                  [reagent "0.7.0"]
+                 [cljsjs/plotly "1.30.0-0"] ; Just using this for the externs
                  [re-frame "0.10.5"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]]
