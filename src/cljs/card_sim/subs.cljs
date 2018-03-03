ffcb54de3a7b6eeb4e678b121b9d3c370c31eead
Mike Richards <mrichards42@gmx.com>
2018-03-03 16:29:31 -0500

    Add heatmap plot
    And reorganize plot views.  Heatmap will be the master view for the
    histogram detail once events are connected.

--- src/cljs/card_sim/subs.cljs
+++ src/cljs/card_sim/subs.cljs
@@ -1,5 +1,6 @@
 (ns card-sim.subs
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [card-sim.stats :as stats]))
 
 (re-frame/reg-sub
  ::deck
@@ -15,8 +16,30 @@
    :x (keys data)
    :y (vals data)})
 
+(defn build-heatmap-trace
+  [data stats-func]
+  {:type "heatmap"
+   :x (map first (keys data))
+   :y (map second (keys data))
+   :z (map stats-func (vals data))})
+
+(def default-stats-func stats/freq-mean)
+(def heatmap-stats-func {:mean stats/freq-mean
+                         :median stats/freq-median})
+
 (re-frame/reg-sub
   ::simulation-graph
   (fn [db]
     {:data [(build-histogram-trace (get-in db [:simulation :bins [0 0]]))]
      :layout {}}))
+
+(re-frame/reg-sub
+  ::simulation-heatmap
+  (fn [db [_ stats-func]]
+    (let [data (get-in db [:simulation :bins])
+          stats-func (get heatmap-stats-func stats-func default-stats-func)]
+      ;; This is a Plotly heatmap map of good cards (x) and bad cards (y),
+      ;; with average round length (z) as the mapped dimension.
+      {:layout {}
+       :data [(build-heatmap-trace data stats-func)]})))
+
--- src/cljs/card_sim/views.cljs
+++ src/cljs/card_sim/views.cljs
@@ -13,10 +13,28 @@
 (defn simulation-histogram
   "A histogram of round-length frequency."
   ([]
-   (simulation-histogram [0 0]))
+   (simulation-histogram [0 0] {}))
   ([bins-key]
+   (simulation-histogram bins-key {}))
+  ([bins-key events]
    (let [graph-data (re-frame/subscribe [::subs/simulation-graph bins-key])]
-     (fn [] [plotly/plot @graph-data])))
+     (fn [] [plotly/plot (into @graph-data events)]))))
+
+(defn simulation-heatmap
+  "A heatmap of average round-length for any point in the round."
+  ([stats-func]
+   (simulation-heatmap stats-func events))
+  ([stats-func events]
+   (let [graph-data (re-frame/subscribe [::subs/simulation-heatmap stats-func])]
+     (fn [] [plotly/plot (into @graph-data events)]))))
+
+(defn simulation-graphs
+  "Combination of heatmap and detail histogram graphs."
+  []
+  (let [detail-key (reagent/atom [0 0])]
+    [:div.graph-container
+     [simulation-heatmap :mean]
+     [simulation-histogram @detail-key]]))
 
 (defn simulation-contols
   []
@@ -44,6 +62,6 @@
   [:div.app
    [:div "All cards" [card/card-list [::subs/deck]]]
    [simulation-contols]
-     [:div "Last round" [card/card-list [::subs/last-round]] ]
-     [simulation-histogram]])
+   [:div "Last round" [card/card-list [::subs/last-round]]]
+   [simulation-graphs]])
 
