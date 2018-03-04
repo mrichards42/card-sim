ae84ea90b283dba88412985f48290330bd126879
Mike Richards <mrichards42@gmx.com>
2018-03-03 19:12:44 -0500

    Add annotations to data points

--- src/cljs/card_sim/subs.cljs
+++ src/cljs/card_sim/subs.cljs
@@ -11,6 +11,45 @@
   ::last-round
   (fn [db] (get-in db [:simulation :last-round])))
 
+;;; Plotly plots
+
+(defn labels-for
+  "Transform data in a trace for textual labels.
+
+  Example:
+
+    (labels-for trace #(.toFixed % 2) [:y])"
+  [trace func data-keys]
+  (let [data ((apply juxt data-keys) trace)]
+    (apply map func data)))
+
+(defn with-labels
+  "Add labels to a Plotly trace."
+  [trace func & data-keys]
+  (let [labels {:text (labels-for trace func data-keys)
+                :textposition "auto"
+                :insidetextfont {:color "white"}}]
+    (merge labels trace)))
+
+(defn annotations-for
+  "Add annotations to a Plotly trace."
+  [trace func & data-keys]
+  ;; Force x and y coords so that the annotation can be placed in the correct
+  ;; location.
+  (letfn [(transform [x y & args] {:text (apply func args)
+                                   :x x
+                                   :y y})]
+    (labels-for trace transform (concat [:x :y] data-keys))))
+
+(defn heatmap-annotations
+  "Build annotations for a heatmap."
+  [trace]
+  (let [default-annotation {:showarrow false
+                            :font {:color "white"}}]
+    (as-> trace t
+      (annotations-for t #(.toPrecision % 2) :z)
+      (map #(merge default-annotation %) t))))
+
 (defn build-histogram-trace
   [data]
   {:type "bar"
@@ -34,11 +73,14 @@
   (fn [db [_ bins-key] [dynamic-bins-key]]
     (let [bins-key (or bins-key dynamic-bins-key [0 0])
           data (get-in db [:simulation :bins bins-key])
+          trace (build-histogram-trace data)
+          n (reduce + (vals data))
+          pct #(-> % (/ n) (* 100) (.toPrecision 3) (str "%"))
           title (str "Round length after "
                      (util/pluralize (first bins-key) " gem")
                      " and " (util/pluralize (second bins-key) " hazard")
                      "<br><i>n = " n "<i>")]
-      {:data [(build-histogram-trace data)]
+      {:data [(with-labels trace pct :y)]
        :layout {:title title}})))
 
 (re-frame/reg-sub
@@ -47,12 +89,15 @@
     (let [data (get-in db [:simulation :bins])
           stats-kw (or stats-func default-stats-func)
           stats-func (get heatmap-stats-func stats-kw)
+          trace (build-heatmap-trace data stats-func)
           title (str (util/ucfirst (name stats-kw))
                      " round length after x gems and y hazards"
                      "<br><i>click to view detail in the histogram</i>")]
       ;; This is a Plotly heatmap map of good cards (x) and bad cards (y),
       ;; with average round length (z) as the mapped dimension.
-      {:data [(build-heatmap-trace data stats-func)]
+      {:data [trace]
        :layout {:title title
                 :xaxis {:title "Gems"}
-                :yaxis {:title "Hazards"}}})))
+                :yaxis {:title "Hazards"}
+                :annotations (heatmap-annotations trace)}})))
+
